f6af714107579c7de5fc9219a2f32f9550b7ab3e
Lucas Guilherme Tascheck <lucasguitascheck@gmail.com>
2023-01-10 18:40:34 -0300

    inscripto test.2

--- inscripto/inscripto.js
+++ inscripto/inscripto.js
@@ -1,18 +1,27 @@
 // const cards = require('cards')
 const game = {
-    table: document.querySelector('.board'),
-    cardContent: `<div class="card"><img class="card-background" src="./img/carta.jpg" width="144px" height="256px" style="position: absolute;" alt=""><img class="heart" src="./img/life.png" width="50px" height="50px" style="position: absolute;" alt=""><img src="./img/claw.png" width="50px" height="50px" style="position: absolute;" alt="" class="claw"><div class="life">3</div><div class="attack">2</div><div class="cost">3</div></div>`,
-    baseLevels: [
-        {id: 0, name:"squirrel", life:1, attack:0, cost:0},
-        {id: 1, name:"wolf", life:3, attack:2, cost:3},
-        {id: 2, name:"shit", life:0, attack:0, cost:0},
-        {id: 3, name:"mantis-god", life:9, attack:9, cost:9}
-    ],
+    baseLevels: [[0, 'squirrel', 1, 0, 0], [1, 'wolf', 3, 2, 2], [2, 'shit', 0, 0, 0], [3, 'mantis-god', 6, 4, 1]],
+    balance: [1, 1, 1, 1, 1],
+    cardPicked: false,
+    cardPlayed: false,
+
+    Card: function(elemen){
+        this.id = elemen[0],
+        this.name = elemen[1],
+        this.life = elemen[2],
+        this.attack = elemen[3],
+        this.cost = elemen[4]
+    },
+
+    levels: {
+        lev_1: [[0, -1, -1, 1], [2, 2, -1, 1]]
+    },
 
     deck: [],
     hand: [0, 0, 0, 0, 0, 0],
     sleep: [],
     cardSelected: -1,
+    temp: [],
 
     board: [[-1, -1, -1, -1], [-1, -1 ,-1, -1], [-1, -1, -1, -1]],
 
@@ -55,10 +64,29 @@ const game = {
         })
     },
 
-    init: function(position){
-        this.baseLevels.forEach(card => {
+    init: function(){
+        for(let i = 0; i < this.baseLevels.length; i++){
+            const card = new this.Card(this.baseLevels[i])
             this.sleep.push(card)
-        })
+        }
+
+        for(let i = 0; i < this.balance.length; i++){
+            document.querySelector(`.balance .weigth${i}`).classList.add('weigth-color')
+        }
+
+        for(let i = 0; i < 4; i++){
+            if(this.levels.lev_1[0][i] != -1){
+                const carta = new this.Card(this.baseLevels[this.levels.lev_1[0][i]])
+                this.putCard(carta, 2, i)
+            }              
+        }
+    },
+
+    initEnemy: function(){
+        for(let i = 0; i < this.baseLevels.length; i++){
+            const card = new this.Card(this.baseLevels[1])
+            this.temp.push(card)
+        }
     },
 
     nextZero: function(array){
@@ -76,6 +104,18 @@ const game = {
             this.hand[nearPos] = carta
             this.putCardOnHand(carta, nearPos)
             this.deleteFromSleep(carta)
+            this.cardPicked = true
+        }
+    },
+
+    pickSquirrel: function(){
+        nearPos = this.nextZero(this.hand)
+        if(nearPos != -1){
+            const carta = new this.Card(this.baseLevels[0])
+            this.hand[nearPos] = carta
+            this.putCardOnHand(carta, nearPos)
+            this.deleteFromSleep(carta)
+            this.cardPicked = true
         }
     },
 
@@ -91,11 +131,10 @@ const game = {
     },
 
     makePlay: function(index){
-        if(this.board[0][index] == -1){
+        if(this.board[0][index] == -1 && this.isVictory() == 0){
             const elem = this.hand[this.cardSelected]
 
             this.putCard(elem, 0, index)
-            console.log(this.board)
 
             document.querySelectorAll('.player-row').forEach(slot => {
                 slot.classList.remove('selectSlot')
@@ -106,15 +145,122 @@ const game = {
             document.querySelector(`.hand .hand${this.cardSelected}`).innerHTML = ''
             this.cardSelected = -1
             this.deleteFromHand(elem)
+            this.cardPlayed = true
         }
     },
 
+    attackAnimation: function(i){
+        document.querySelector(`.slot0${i} .card`).classList.add('attackAnime')
+        setTimeout(() => {
+            document.querySelector(`.slot0${i} .card`).classList.remove('attackAnime')
+        }, 500)
+    },
+
+    attackAnimationEnemy: function(i){
+        document.querySelector(`.slot1${i} .card`).classList.add('attackAnimeEnemy')
+        setTimeout(() => {
+            document.querySelector(`.slot1${i} .card`).classList.remove('attackAnimeEnemy')
+        }, 500)
+    },
+
+
     passTurn: function(){
+        if(this.isVictory() != 0 || this.cardPicked == false){return}
+        const attackDamage = 0
         for(let i = 0; i < 4; i++){
-            if(this.board[1][i] != -1 && this.board[0][i] != -1){
+            if(this.board[1][i] != -1 && this.board[0][i] != -1 && this.board[0][i].attack != 0){
                 this.board[1][i].life -= this.board[0][i].attack
-                this.putCard(this.board[1][i], 1, i)    
+                this.attackAnimation(i)
+                if(this.board[1][i].life <= 0){
+                    this.board[1][i].life = 0
+                    this.putCard(this.board[1][i], 1, i)  
+                    setTimeout(() => {
+                        this.board[1][i] = -1
+                        document.querySelector(`.slot1${i}`).innerHTML = ''
+                    }, 1000)
+                }else{
+                    this.putCard(this.board[1][i], 1, i)
+                }
+            }
+            if(this.board[1][i] == -1 && this.board[0][i] != -1 && this.board[0][i].attack != 0){
+                this.attackAnimation(i)
+                this.addBalance(this.board[0][i].attack)
             }
         }
+        if(this.isVictory() != 0){return}
+        setTimeout(()=>{
+            for(let i = 0; i < 4; i++){
+                if(this.board[1][i] != -1 && this.board[0][i] != -1 && his.board[1][i].attack != 0){
+                    this.board[0][i].life -= this.board[1][i].attack
+                    this.attackAnimationEnemy(i)
+                    if(this.board[0][i].life <= 0){
+                        this.board[0][i].life = 0
+                        this.putCard(this.board[0][i], 0, i)  
+                        setTimeout(() => {
+                            this.board[0][i] = -1
+                            document.querySelector(`.slot0${i}`).innerHTML = ''
+                        }, 1000)
+                    }else{
+                        this.putCard(this.board[0][i], 0, i)  
+                    }
+                }
+                if(this.board[0][i] == -1 && this.board[1][i] != -1 && this.board[1][i].attack != 0){
+                    this.attackAnimationEnemy(i)
+                    this.removeBalance(this.board[1][i].attack)
+                }
+            }
+            this.cardPicked = false
+        }, 2000)
+        if(this.isVictory() != 0){return}
+    },
+
+    clearBalance: function(){
+        for(let i = 0; i < 10; i++){
+            document.querySelector(`.balance .weigth${i}`).classList.remove('weigth-color')
+        }
+    },  
+
+    addBalance: function(num){
+        if((this.balance.length + num) >= 10){
+            for(let i = 0; i < 10; i++){
+                document.querySelector(`.balance .weigth${i}`).classList.add('weigth-color')
+            }
+            this.balance = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+            return
+        }
+        for(let i = 0; i < num; i++){
+            this.balance.push(1)
+        }
+        this.clearBalance()
+        for(let i = 0; i < this.balance.length; i++){
+            document.querySelector(`.balance .weigth${i}`).classList.add('weigth-color')
+        }
+    },
+
+    removeBalance: function(num){
+        if((this.balance.length - num) <= 0){
+            this.clearBalance()
+            this.balance = []
+            return
+        }
+        for(let i = 0; i < num; i++){
+            this.balance.pop()
+        }
+        this.clearBalance()
+        for(let i = 0; i < this.balance.length; i++){
+            document.querySelector(`.balance .weigth${i}`).classList.add('weigth-color')
+        }
+    },
+
+    isVictory: function(){
+        if(this.balance.length == 0){
+            console.log('DERROTA!')
+            return -1
+        }
+        if(this.balance.length == 10){
+            console.log('VITORIA!')
+            return 1
+        }
+        return 0
     },
 }
